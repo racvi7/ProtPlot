e9f99eac9cdc82cda7866e8fa72c56d7c4cb87fa
racvi7 <169043962+racvi7@users.noreply.github.com>
2025-05-01 22:39:23 +0200

    added 3d plot and 3d interactive plot

--- ProteinPlot/protplot.py
+++ ProteinPlot/protplot.py
@@ -25,9 +25,12 @@ Be aware that this package was made for a University project in Hungary, PPKE IT
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 import urllib.request as urllib2
 import io
+
+
 
 
 #-------------------------------------FUNCTIONS-----------------------------------------------------
@@ -172,8 +175,170 @@ def plot_projection(protein_df, colorcode = 'default', atoms = 'all',
     
 #plot_projection(example2, alpha = 0.55,aminos = ['CYS'], atoms = ['CA', 'SG'], 
                 #Title = 'Test projection plot', figsave = 'test.jpg')
+
+#---------------------------------------------------------------------------------------------------
+# Function - plot_structure_3d()
+
+def plot_structure_3d(
+    protein_df1,
+    protein_df2=None,
+    atoms1='all', atoms2='all',
+    aminos1='all', aminos2='all',
+    colorcode1='default', colorcode2='default',
+    alpha1=1.0, alpha2=1.0,
+    marker1='o', marker2='x',
+    marker_size1=20, marker_size2=20,
+    fig_size=(10, 10),
+    Title=None, figsave=None
+):
+    """
+    3D scatter plot of one or two protein structures using matplotlib.
+    """
+
+    def plot_single_structure(ax, df, atoms, aminos, colorcode, alpha, marker, marker_size, label_prefix):
+        plot_df = df.copy()
+
+        if atoms != 'all':
+            plot_df = plot_df[plot_df.atom_type.isin(atoms)]
+
+        if aminos != 'all':
+            plot_df = plot_df[plot_df.amino_type.isin(aminos)]
+
+        if colorcode == 'default':
+            ccode = {'C': 'black', 'N': 'blue', 'O': 'red', 'S': 'yellow'}
+        else:
+            ccode = colorcode
+
+        plot_df['defined_atoms'] = plot_df.atom_type.str[0]
+        dpg = plot_df.groupby('defined_atoms')
+
+        for dat, plotter in dpg:
+            if dat in ccode:
+                ax.scatter(plotter.x.values, plotter.y.values, plotter.z.values,
+                           color=ccode[dat],
+                           label=f"{label_prefix}: {dat}",
+                           alpha=alpha,
+                           marker=marker,
+                           s=marker_size)
+
+    # Create plot
+    fig = plt.figure(figsize=fig_size)
+    ax = fig.add_subplot(111, projection='3d')
+
+    plot_single_structure(ax, protein_df1, atoms1, aminos1, colorcode1, alpha1, marker1, marker_size1, 'P1')
+
+    if protein_df2 is not None:
+        plot_single_structure(ax, protein_df2, atoms2, aminos2, colorcode2, alpha2, marker2, marker_size2, 'P2')
+
+    ax.set_xlabel('X', fontsize=14)
+    ax.set_ylabel('Y', fontsize=14)
+    ax.set_zlabel('Z', fontsize=14)
+
+    if Title:
+        ax.set_title(Title, fontsize=18)
+
+    ax.legend(title='Atom Type')
     
+    if figsave:
+        plt.savefig(figsave, dpi=300)
+
+    plt.tight_layout()
+    plt.show()
+
+#example usage:
+
+#example2 = protplot.read_pdb('6vxx')
+#example = protplot.read_pdb('7v7n')
+
+#plot_structure_3d(example, example2, atoms1=['CA'], atoms2=['CA'], colorcode1={'C': 'green'}, colorcode2={'C': 'red'}, alpha1=0.9, alpha2=0.5, marker1='o', marker2='^',marker_size1=40, marker_size2=60)
+
+#---------------------------------------------------------------------------------------------------
+#Function - plot_structure_3d()
+# This function is an interactive version of the plot_structure_3d function using Plotly.
+
+
+def plot_structure_3d_interactive(
+    protein_df1,
+    protein_df2=None,
+    atoms1='all', atoms2='all',
+    aminos1='all', aminos2='all',
+    colorcode1='default', colorcode2='default',
+    alpha1=1.0, alpha2=1.0,
+    marker1='circle', marker2='circle',
+    marker_size1=5, marker_size2=5,
+    fig_width=1000, fig_height=1000,
+    Title=None, figsave=None
+):
+    """
+    Interactive 3D scatter plot of one or two proteins, each customizable.
+    """
+
+    fig = go.Figure()
+
+    def add_structure_to_plot(df, atoms, aminos, colorcode, alpha, marker, marker_size, label_prefix):
+        plot_df = df.copy()
+
+        if atoms != 'all':
+            plot_df = plot_df[plot_df.atom_type.isin(atoms)]
+
+        if aminos != 'all':
+            plot_df = plot_df[plot_df.amino_type.isin(aminos)]
+
+        if colorcode == 'default':
+            ccode = {'C': 'black', 'N': 'blue', 'O': 'red', 'S': 'yellow'}
+        else:
+            ccode = colorcode
+
+        plot_df['defined_atoms'] = plot_df.atom_type.str[0]
+
+        for atom_type in plot_df['defined_atoms'].unique():
+            subset = plot_df[plot_df['defined_atoms'] == atom_type]
+            fig.add_trace(go.Scatter3d(
+                x=subset.x,
+                y=subset.y,
+                z=subset.z,
+                mode='markers',
+                marker=dict(
+                    size=marker_size,
+                    color=ccode.get(atom_type, 'gray'),
+                    opacity=alpha,
+                    symbol=marker
+                ),
+                name=f"{label_prefix}: {atom_type}"
+            ))
+
+    # Add first structure
+    add_structure_to_plot(protein_df1, atoms1, aminos1, colorcode1, alpha1, marker1, marker_size1, 'P1')
+
+    # Optional second structure
+    if protein_df2 is not None:
+        add_structure_to_plot(protein_df2, atoms2, aminos2, colorcode2, alpha2, marker2, marker_size2, 'P2')
+
+    fig.update_layout(
+        title=Title or '3D Protein Structure(s)',
+        scene=dict(
+            xaxis_title='X',
+            yaxis_title='Y',
+            zaxis_title='Z'
+        ),
+        legend_title='Atom Type',
+        width=fig_width,
+        height=fig_height
+    )
+
+    if figsave:
+        fig.write_html(figsave)
+
+    fig.show()
     
+
+#example usage:
+#example2 = protplot.read_pdb('6vxx')
+#example = protplot.read_pdb('7v7n')
+
+#plot_structure_3d_interactive(example, example2, atoms1=[CA], atoms2=['CA'],colorcode1={'C': 'blue'}, colorcode2={'C': 'red'},marker_size1=4, marker_size2=6,alpha1=0.9, alpha2=0.4)
+
+
     
     
     
