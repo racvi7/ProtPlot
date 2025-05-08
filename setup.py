4cf570481788e759ff5c23ca86f36ee0d96006d0
racvi7 <169043962+racvi7@users.noreply.github.com>
2025-05-08 11:11:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     url="https://protplot.readthedocs.io/en/latest/",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: Other/Proprietary License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",)
