6a6a6717cf0fbf2b33811c20ffd2378fdf0af53d
Dénes Döme <denes.dome@gmail.com>
2025-05-09 11:11:51 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,11 +19,6 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
-}
-
 
 
 # -- Options for HTML output -------------------------------------------------
